55779b6834d92c437ec741b6ca461d29f57f1217
Eric904P <bookworm904@gmail.com>
2018-02-04 13:39:55 -0500

    Update HK_Soraka.lua

--- HK_Soraka.lua
+++ HK_Soraka.lua
@@ -152,18 +152,18 @@ end
 function Heal()
 	local needHeal = 0
 	for _, hero in pairs(GetAllyHeroes()) do
-		if hero ~= myHero and ValidTarget(hero, Spells.W.range) and (GetMaxHP(hero) - GetCurrentHP(hero)) >= wHeal() and (GetCurrentMana(myHero)/GetMaxMana(myHero)) >= (Menu.Heal.Mana:Value()/100) then
+		if hero ~= myHero and ValidTarget(hero, Spells.W.range) and (GetCurrentHP(hero)/GetMaxHP(hero)) >= 0.8 and (GetCurrentMana(myHero)/GetMaxMana(myHero)) >= (Menu.Heal.Mana:Value()/100) then
 			castTargetSpell(hero, _W)
 		end
-		if hero and (Menu.Heal.RHP:Value()/100) >= (GetCurretnHP(hero)/GetMaxHP(hero)) then
+		if hero and (Menu.Heal.RHP:Value()/100) >= (GetCurrentHP(hero)/GetMaxHP(hero)) then
 			needHeal = (needHeal + 1)
 			if hero == myHero and Menu.Heal.RSelf:Value() then
-				CastR()
+				castR()
 			end
 		end
 	end
 	if needHeal >= Menu.Heal.RCount:Value() then
-		CastR()
+		castR()
 	end
 end
 
