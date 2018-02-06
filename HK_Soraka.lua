07b016d7f4e563e7e0ebd19d9344b8de8dc91ee2
Lewis Curl <bookworm904@gmail.com>
2018-02-05 19:40:28 -0500

    heal
    added IsObjectAlive

--- HK_Soraka.lua
+++ HK_Soraka.lua
@@ -152,7 +152,7 @@ end
 function Heal()
 	local needHeal = 0
 	for _, hero in pairs(GetAllyHeroes()) do
-		if Menu.Heal.AutoW:Value() and (GetCurrentMana(myHero)/GetMaxMana(myHero)) >= (Menu.Heal.Mana:Value()/100)  then
+		if IsObjectAlive(myHero) and Menu.Heal.AutoW:Value() and (GetCurrentMana(myHero)/GetMaxMana(myHero)) >= (Menu.Heal.Mana:Value()/100)  then
 			if Ready(_W) and hero ~= myHero and ValidTarget(hero, 550) and (GetCurrentHP(hero)/GetMaxHP(hero)) <= 0.8 then
 				castTargetSpell(hero, _W)
 			end
@@ -161,11 +161,11 @@ function Heal()
 			needHeal = (needHeal + 1)
 			if hero == myHero and Menu.Heal.RSelf:Value() and Ready(_R) then
 				castR()
-			elseif Ready(_R) and (GetCurrentHP(myHero)/GetMaxHP(myHero)) <= (Menu.Heal.RHP:Value()/100) and Menu.Heal.RSelf:Value() then
-				castR()
 			end
+		elseif Ready(_R) and IsObjectAlive(myHero) and (GetCurrentHP(myHero)/GetMaxHP(myHero)) <= (Menu.Heal.RHP:Value()/100) and Menu.Heal.RSelf:Value() then
+			castR()
 		end
-		if needHeal >= Menu.Heal.RCount:Value() and Ready(_R) then
+		if needHeal >= Menu.Heal.RCount:Value() and Ready(_R) and IsObjectAlive(myHero) then
 			castR()
 		end
 	end
